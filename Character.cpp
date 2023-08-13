0f7ed4ac05f11afd97ffcde5ecaf19c22200a025
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-08-13 23:22:44 +0300

    add character class

---
+++ Character.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 13/08/2023.
+//
+
+#include "Character.h"
---
+++ Character.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 13/08/2023.
+//
+
+#ifndef MATRIX_CODE_RAIN_CHARACTER_H
+#define MATRIX_CODE_RAIN_CHARACTER_H
+
+
+class Character {
+
+};
+
+
+#endif //MATRIX_CODE_RAIN_CHARACTER_H
