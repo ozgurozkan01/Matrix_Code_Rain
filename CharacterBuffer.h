a83fc649636d03ebdb1ee8719590b2b937e2088c
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-08-14 21:32:00 +0300

    add character buffer class

---
+++ CharacterBuffer.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 14/08/2023.
+//
+
+#ifndef MATRIX_CODE_RAIN_CHARACTERBUFFER_H
+#define MATRIX_CODE_RAIN_CHARACTERBUFFER_H
+
+
+class CharacterBuffer {
+
+};
+
+
+#endif //MATRIX_CODE_RAIN_CHARACTERBUFFER_H
